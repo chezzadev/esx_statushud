7176a9061d20f1deac11238e92f444fd8c4ae2cc
Chezz <bencherrington28@gmail.com>
2020-01-05 01:50:43 +0000

    Add onTick

---
+++ html/script.js
@@ -0,0 +1,18 @@
+$(function(){
+    window.addEventListener("message", function(event){
+        if (event.data.pauseMenu == false) {
+            $(".ui").fadeIn();
+            var health = event.data.health;
+            $("#health-percent").html(Math.round(health) + "%");
+            $("#health-level").css("width", health + "%");
+            var food = event.data.food;
+            $("#food-percent").html(Math.round(food) + "%");
+            $("#food-level").css("width", food + "%");
+            var water = event.data.water;
+            $("#water-percent").html(Math.round(water) + "%");
+            $("#water-level").css("width", water + "%");
+        } else {
+            $(".ui").fadeOut();
+        }
+    })
+})
